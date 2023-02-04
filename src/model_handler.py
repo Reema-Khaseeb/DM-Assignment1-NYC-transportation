4ddde20f8981099a004976502b40a210198f820f
mohamad <s11924310@stu.najah.edu>
2023-02-05 01:05:34 +0200

    fix the return problem

--- src/model_handler.py
+++ src/model_handler.py
@@ -5,12 +5,11 @@ This file could contain functions for loading and saving models, as well as func
 
 def load_model(model_path):
     # code to load a pre-trained model from model_path
-    return model
+    pass
 
 def train_model(model, train_data, train_labels):
     # code to train the model on train_data and train_labels
-    return trained_model
-
+    pass
 def evaluate_model(model, test_data, test_labels):
     # code to evaluate the model on test_data and test_labels
-    return accuracy
+    pass
